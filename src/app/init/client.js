5e7423a73d4e4d6f58bbfa9fb33a0f11ec91e492
Павел <58857483+Haskiro@users.noreply.github.com>
2023-01-30 13:35:59 +0300

    client settings was rewrited for support subscriptions due to server settings

--- src/app/init/client.js
+++ src/app/init/client.js
@@ -1,19 +1,22 @@
 import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
 // GraphQL server
 const root = "funded-pet-library.moonhighway.com/";
 
+// HTTP
 const uri = `https://${root}`;
 const httpLink = new HttpLink({
 	uri,
 });
-const wsLink = new GraphQLWsLink(
-	createClient({
-		uri: `wss://${root}graphql`,
+
+// WS
+const wsLink = new WebSocketLink(
+	new SubscriptionClient(`wss://${root}graphql`, {
+		reconnect: true,
 	})
 );
 
